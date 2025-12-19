43a05ea1e80def2b9af0c5c649072fb4f7e777ed
Nigolas <65961519+Naturaldetective@users.noreply.github.com>
2025-12-19 16:11:11 +0800

    Change default GitHub repo name to 'blog'

--- src/consts.ts
+++ src/consts.ts
@@ -9,7 +9,7 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  */
 export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Nigolas',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'BLOG',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2499921',
 } as const
